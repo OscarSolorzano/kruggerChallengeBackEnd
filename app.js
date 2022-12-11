91274b4104ef5d490c46ebfb85f3467975a7551f
Oscar Solorzano <oscar.solorzano98@gmail.com>
2022-12-11 05:26:00 -0500

    CORS enable

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 // Routers
 const { usersRouter } = require('./routes/users.routes');
@@ -12,6 +16,18 @@ const app = express();
 // Enable Express app to receive JSON data
 app.use(express.json());
 
+// Enable cors
+app.use(cors());
+
+// Add security headers
+app.use(helmet());
+
+// Compress responses
+app.use(compression());
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else if (process.env.NODE_ENV === 'production') app.use(morgan('combined'));
+
 // Define endpoints
 app.use('/api/v1/users', usersRouter);
 
@@ -20,10 +36,10 @@ app.use(globalErrorHandler);
 
 // Catch non-existing endpoints
 app.all('*', (req, res) => {
-	res.status(404).json({
-		status: 'error',
-		message: `${req.method} ${req.url} does not exists in our server`,
-	});
+  res.status(404).json({
+    status: 'error',
+    message: `${req.method} ${req.url} does not exists in our server`,
+  });
 });
 
 module.exports = { app };
